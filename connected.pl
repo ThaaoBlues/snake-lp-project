aec5336a3d5c18d602c841a6f92d1bd6e1266ef6
irisB0 <150732526+irisB0@users.noreply.github.com>
2025-05-21 20:06:27 +0200

    Create file for connected snake

---
+++ connected.pl
@@ -0,0 +1,7 @@
+% snakeConnected(Solution)
+
+% get a head and a tail
+% start at the head
+% go to the next cell in the snake
+%     -> get neighbors and keep track of where you came from
+% until you arrive at the tail
