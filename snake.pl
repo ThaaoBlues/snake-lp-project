391db10a62aec9687f41c1b72b33037c7b863016
thaaoblues <thaaoblues81@gmail.com>
2025-05-26 16:16:18 +0200

    fix : cosmetic + one file merge

--- snake.pl
+++ snake.pl
@@ -1,19 +1,93 @@
-%
-% PASTE THIS TWO LINES IN SWIPL BEFORE
-%
-%use_module(library(clpfd)).
-%set_prolog_flag(clpfd_monotonic, true). % setting to get useful errors sometimes
+checkRowClues([], []).
+checkRowClues([_|Grid], [-1|Clues]) :- !, checkRowClues(Grid, Clues).
+checkRowClues([Row|Grid], [Clue|Clues]) :- 
+    countRow(Row, Count),
+    Count #= Clue,
+    checkRowClues(Grid, Clues).
+
+countRow([], 0).
+countRow([Val|Row], Count) :-
+    ((Val #= 1) #\/ (Val #= 2)),
+    countRow(Row, C),
+    Count is C + 1.
 
 
+countRow([Val|Row], Count) :-
+    Val #= 0,
+    countRow(Row, Count).
 
-% TO IMPLEMENT
-% checkRowClues()
-% checkColClues()
-% nonTouching() % check diagonals ?? (what is this function supposed to do ?)
-% countNeighbors() % don't check diagonals
-% snakeConnected()
+checkColClues([],[]).
+checkColClues(Grid, Clues) :-
+    transpose(Grid, Trans),
+    checkRowClues(Trans, Clues).
 
 
+% no hints
+test1(S) :- copyGrid([[ 1, 1],[-1,-1]],S),
+            % testRowRules([-1,-1],S), 
+            % testColRules([-1,-1],S), 
+            print_only_grid(S).
+
+
+% must have 1 on row 1 of the grid and 2 (one or 2) on the first column, multiple solutions
+test2(S) :- copyGrid([[-1,-1, 1],[-1,-1,-1],[ 1,-1,-1]],S),
+             maplist(label,S), % force variable instanciation
+             checkRowClues(S,[ 1,-1,-1]), 
+             checkColClues(S,[ 2,-1,-1]),
+            print_only_grid(S),
+            nl.
+snakeConnected(Solution) :-
+    getHead(Solution, H),
+    nextSnake(Solution, [], H, Path),
+    length(Path, Length),
+    Snake is Length,
+    count(Solution, Count),
+    Snake = Count, !.
+
+head(cell(_,_,1)).
+
+getHead(Grid, cell(X,Y,1)) :-
+    nth0(Y, Grid, Row),
+    nth0(X, Row, 1), !.
+
+getValue(Grid, cell(X,Y,V)) :-
+    nth0(Y, Grid, Row),
+    nth0(X, Row, V), !.
+
+neighboring(cell(Xc,Y,_), cell(Xn,Y,_)) :-
+    Xn is Xc + 1;
+    Xn is Xc - 1.
+
+neighboring(cell(X,Yc,_), cell(X,Yn,_)) :-
+    Yn is Yc + 1;
+    Yn is Yc - 1.
+
+nextSnake(Grid, _, Cell, [Cell]) :- 
+    head(Cell), 
+    getHead(Grid, H),
+    not(Cell = H), !.
+nextSnake(Grid, Visited, Current, [Current, cell(Xn,Yn,V)|Snake]) :-
+    neighboring(Current, cell(Xn,Yn,V)),
+    getValue(Grid, cell(Xn, Yn, V)),
+    (V = 1 ; V = 2),
+    not(member(cell(Xn,Yn,V), Visited)),
+    nextSnake(Grid, [Current|Visited], cell(Xn,Yn,V), [cell(Xn,Yn,V)|Snake]).
+
+count([], 0).
+count([Row|Grid], Count) :-
+    count(Grid, C),
+    rowCount(Row, Cr),
+    Count is C + Cr.
+
+rowCount([], 0).
+rowCount([1|Row], Count) :- !,
+    rowCount(Row, C),
+    Count is C + 1.
+rowCount([2|Row], Count) :- !,
+    rowCount(Row, C),
+    Count is C + 1.
+rowCount([_|Row], Count) :-
+    rowCount(Row, Count).
 
 goodGrid([]).
 goodGrid([Row|Grid]) :-
@@ -44,9 +118,123 @@ countHeadRow([Val|Row], Count) :-
     Val #\= 1,
     countHeadRow(Row, Count).
 
-:- dynamic(solution_cache/2).
+nonTouching([Row1, Row2]) :- nonTouching_rows(Row1, Row2), !.
 
-snake(_,_,G,S) :-solution_cache(G,S),write("Cache hit\n").
+nonTouching([Row1, Row2 | Rows]) :-
+    nonTouching_rows(Row1, Row2),
+    nonTouching([Row2 | Rows]).
+
+%base case
+nonTouching_rows(R1, R2) :- 
+    block_of_2([R1,R2]), % no 4 block of 2s (technically induced by no diag ?)
+    nothing_in_diag_of_1([R1,R2]),!. % no head/tail with something in diagonal
+
+nonTouching_rows([A1, A2 | AList], [B1, B2 | BList]) :- 
+    block_of_2([[A1,A2],[B1,B2]]), % no 4 block of 2s (technically induced by no diag ?)
+    nothing_in_diag_of_1([[A1,A2],[B1,B2]]), % no head/tail with something in diagonal
+    diagonals_of_two([[A1,A2],[B1,B2]]),
+    nonTouching_rows([A2 | AList], [B2 | BList]).
+
+
+
+% constrained version, unlabeled variables
+block_of_2([[A1,A2],[B1,B2]]) :- ((A1 #\= 2) #\/ (A2 #\= 2) #\/ (B1 #\= 2) #\/ (B2 #\= 2)).
+    
+
+diagonals_of_two([[A1,A2],[B1,B2]]) :- 
+    ((A1 #= 2) #/\ (A2 #= 0) #/\ (B1 #= 0) #==> (B2 #= 0)),
+    ((A2 #= 2) #/\ (A1 #= 0) #/\ (B2 #= 0) #==> (B1 #= 0)).
+
+
+% constrained version, unlabeled variables
+nothing_in_diag_of_1([[A1, A2], [B1, B2]]) :-
+    % Diagonals: A1-B2 and A2-B1
+    ((A1 #= 1) #/\ (A2 #= 0) #/\ (B1 #= 0) #==> (B2 #= 0)),
+    ((A2 #= 1) #/\ (A1 #= 0) #/\ (B2 #= 0) #==> (B1 #= 0))
+    ,!
+    .
+
+% accept the case where no 1 is on the 4x4 chunk
+nothing_in_diag_of_1([[A1,A2],[B1,B2]]) :- A1 #\=1, A2 #\=1, B1 #\=1, B2 #\=1.
+
+
+extend_grid(OldGrid,NewGrid) :-
+    transpose(OldGrid,TransGrid),
+    extend_grid_rows(TransGrid,RowTransGrid),
+    transpose(RowTransGrid,RowGrid),
+    extend_grid_rows(RowGrid,NewGrid).
+
+%base case
+extend_grid_rows([Row], [NewGrid]) :-
+    extend_row(Row, NewGrid). 
+
+extend_grid_rows([Row1 | Rows], [NewRow1 | NewRows]) :-
+    extend_row(Row1, NewRow1),
+    extend_grid_rows(Rows, NewRows).
+
+% Extend a row by adding a 0 at both ends
+extend_row(OldRow,NewRow) :- append([0|OldRow],[0],NewRow).
+
+undo_extend(OldGrid, NewGrid) :-
+    undo_extend_rows(OldGrid, RowGrid),
+    transpose(RowGrid, RowTransGrid),
+    undo_extend_rows(RowTransGrid, TransGrid),
+    transpose(TransGrid, NewGrid).
+
+%base case
+undo_extend_rows([Row], [NewGrid]) :-
+    undo_extend_row(Row, NewGrid).
+
+undo_extend_rows([Row1 | Rows], [NewRow1 | NewRows]) :-
+    undo_extend_row(Row1, NewRow1),
+    undo_extend_rows(Rows, NewRows).
+
+undo_extend_row([0 | TailRow], NewRow) :- deleteLastElement(TailRow, NewRow). 
+
+deleteLastElement(TailRow, NewRow) :- 
+    reverse(TailRow, [0|ReverseRow]),
+    reverse(ReverseRow, NewRow).
+
+check_neighbors_pattern(0,_,_,_,_).
+check_neighbors_pattern(Piece,N,E,S,W) :- 
+    1 #=< Piece,
+    count_cell(N,X1),
+    count_cell(E,X2),
+    count_cell(S,X3),
+    count_cell(W,X4),
+    %write(Piece),
+    %count_piece_cell(Piece, Max_Neighbours),
+    Piece #= X1+X2+X3+X4.
+
+
+
+% scan the grid rows 3 by 3 and check the neighbors of each middle case of row B
+% then put the count variable in the actual grid case that was scanned ( so will mostly put 2,0 or 1)
+% this do not check diagonals
+% rowA  #?#
+% rowB  ?*?
+% rowC  #?#
+
+%base
+check_neighbors_rows([_,A2], [B1,B2], [_, C2]) :-
+    check_neighbors_pattern(B2, A2, 0, C2, B1).
+
+check_neighbors_rows([_,N,A3|RowA],[W,M,E|RowB],[_,S,C3|RowC]) :-
+    check_neighbors_pattern(M,N,E,S,W),
+    check_neighbors_rows([N,A3|RowA],[M,E|RowB],[S,C3|RowC]).
+
+countNeighbors2([Row1, Row2, Row3]) :- check_neighbors_rows(Row1, Row2, Row3).
+
+countNeighbors2([Row1, Row2, Row3 | Rows]) :-
+    check_neighbors_rows(Row1, Row2, Row3),
+    countNeighbors2([Row2, Row3 | Rows]).
+
+
+countNeighbors(Solution) :-
+    extend_grid(Solution, Extended)
+    , countNeighbors2(Extended)
+    , undo_extend(Extended, Solution)
+    .
 
 snake(RowClues, ColClues, Grid, Solution):-
     %write("copyGrid\n"),
@@ -57,18 +245,14 @@ snake(RowClues, ColClues, Grid, Solution):-
     checkRowClues(Solution, RowClues),
     %write("checkColClues\n"),
     checkColClues(Solution, ColClues),
-    %write("nonTouching\n"),
-    nonTouching(Solution),
     %write("countNeighbors\n"),
     countNeighbors(Solution),
+    %write("nonTouching\n"),
+    nonTouching(Solution),
     %write("snakeConnected\n"),
-    snakeConnected(Solution),
-    %write("labeling\n"),
-    maplist(label, Solution)
-    %\+ solution_cache(Grid,Solution),
-    %assertz(solution_cache(Grid,Solution)),
-.
+    snakeConnected(Solution)
 
+.
 
 copyGrid([],[]).
 copyGrid([Row|G],[RowS|S]) :- copyRow(Row,RowS), copyGrid(G,S).
@@ -81,9 +265,7 @@ copyRow([-1|R],[Cell_Value|S]) :-
                                 , !.
 copyRow([Clue|R],[Clue|S]) :- copyRow(R,S).
 
-%count_cell(0,0).
-%count_cell(1,1).
-%count_cell(2,1).
+
 count_cell(Cell, Count) :-
     % Count is 1 if Cell is 1 or 2, else 0
     % that's so fancy we have <==> to mean if and only if
